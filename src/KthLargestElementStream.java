fd7f6589ddaa75f544c1d3472d19d356f2e7cea6
Jatish Khanna <jatish.khanna@gmail.com>
2025-01-19 10:13:12 -0700

    Kth Largest in the Stream - with PQ

--- src/KthLargestElementStream.java
+++ src/KthLargestElementStream.java
@@ -1,4 +1,26 @@
+import java.util.PriorityQueue;
+
 public class KthLargestElementStream {
+
+  KthLargestElementStream(int size) {
+    this.size = size;
+  }
+
+  PriorityQueue<Integer> stream = new PriorityQueue<>();
+  int size = 0;
+
+  private Integer getKthLargest() {
+    return stream.isEmpty() || stream.size() < size ? -1 : stream.peek();
+  }
+
+  private void add(int data) {
+    if (stream.size() < size) {
+      stream.offer(data);
+    } else if (!stream.isEmpty() && stream.peek() < data) {
+      stream.poll();
+      stream.offer(data);
+    }
+  }
 
   public static void main(String[] args) {
     KthLargestElementStream stream = new KthLargestElementStream(3);
@@ -23,4 +45,6 @@ public class KthLargestElementStream {
     stream.add(3);
     System.out.println("The 3rd largest element: " + stream.getKthLargest());
   }
+
+
 }
