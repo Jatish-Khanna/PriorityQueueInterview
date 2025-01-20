f2dff1b5840fb2fd06d51c0326507efc5a3f88f2
Jatish Khanna <jatish.khanna@gmail.com>
2025-01-20 07:35:19 -0700

    Arrange Data by freq - alternating to be unqiue

--- src/CreateWithAlternatingSequence.java
+++ src/CreateWithAlternatingSequence.java
@@ -1,7 +1,41 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class CreateWithAlternatingSequence {
 
 
+  private static String reorganizeString(String s) {
+    if (s == null) {
+      return "";
+    }
+    char[] freq = new char[26];
+    for (char data : s.toCharArray()) {
+      freq[data - 'a']++;
+    }
 
+    PriorityQueue<int[]> order = new PriorityQueue<>(Comparator.comparing(d -> -d[1]));
+    for (int index = 0; index < freq.length; index++) {
+      if (freq[index] > 0) {
+        order.offer(new int[]{index, freq[index]});
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    int[] prev = null;
+    int[] current;
+    while (!order.isEmpty()) {
+      current = order.poll();
+      sb.append((char)(current[0] + 'a'));
+      --current[1]; // update the freq
+
+      if (prev != null && prev[1] > 0) {
+        order.offer(prev);
+      }
+      prev = current;
+    }
+
+    return sb.length() == s.length() ? sb.toString() : "";
+  }
 
   public static void main(String[] args) {
     String s = "aaabbc";
@@ -12,5 +46,11 @@ public class CreateWithAlternatingSequence {
     } else {
       System.out.println("Reorganized String: " + result);
     }
+
+
+    System.out.println("Reorganized String: " + reorganizeString("aaaaa"));
+    System.out.println("Reorganized String: " + reorganizeString("asdfhlycajnkaweisrjfak"));
+
+
   }
 }
